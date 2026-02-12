4ad12d31c5310a8efa3bea796c0cbacfd6f6ca67
drvova <drvova@openwarp.local>
2026-02-12 12:09:06 -0600

    Trust reverse proxy headers for correct HTTPS URL generation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->throttleApi('api', true);
         $middleware->authenticateSessions();
         $middleware->statefulApi();
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
 
         $middleware->trimStrings(
             except: [
